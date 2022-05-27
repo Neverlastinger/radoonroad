0ff60dd0efa74fb373f3237eb3dce76aa33d0741
Neverlastinger <shadow_87@mail.bg>
2022-05-27 16:45:30 +0300

    Add article page

---
+++ pages/[slug].js
@@ -0,0 +1,31 @@
+import Article from 'components/Article';
+import Page from 'components/Page';
+import { fetchArticleBySlug } from 'fetch';
+import Custom404 from 'pages/404';
+
+function ArticlePreviewPage({ article, errors }) {
+  if (!article || errors) {
+    return <Custom404 errors={errors} />;
+  }
+
+  return (
+    <Page>
+      <Article article={article} />
+    </Page>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const { article, errors } = await fetchArticleBySlug({
+    slug: params.slug
+  });
+
+  return {
+    props: {
+      article,
+      errors
+    }
+  };
+}
+
+export default ArticlePreviewPage;
